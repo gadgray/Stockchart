1204aaf529c4e6f3e75552010e51add1151ad5ac
gadgray <71590466+gadgray@users.noreply.github.com>
2023-08-21 11:47:06 +0100

    logo add

--- src/pages/StockOverview.js
+++ src/pages/StockOverview.js
@@ -1,11 +1,13 @@
 import AutoComplete from "../components/AutoComplete";
 import StockList from "../components/StockList";
-
+import logo from '../Logo/pngwing.com.png'
+// import {Image} from 'react'
 function StockOverview(){
 
     return(
-        <div>
-            <h2>Root Route</h2>
+        <div className="d-flex flex-column w-100 justify-contents-center">
+            <h2 className="">StockOverview</h2>
+            <img src={logo} className="w-50 align-self-center my-4" alt="stock Logo"></img>
             <AutoComplete />
             <StockList />
         </div>
